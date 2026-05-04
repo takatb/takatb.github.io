a666e228a06db4316b3192f07b85ca83d394d474
Takat B <tac_m@msn.com>
2026-05-05 04:16:50 +0700

    Render apps index with Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy({
-    "src/pages": "apps"
+    "src/pages/mass_spring_simulation.html": "apps/mass_spring_simulation.html",
+    "src/pages/pendulum_simulation.html": "apps/pendulum_simulation.html",
+    "src/pages/qr_gen.html": "apps/qr_gen.html",
+    "src/pages/893af8a5-ab82-4be0-bca4-c7c2d84df4cc.png": "apps/893af8a5-ab82-4be0-bca4-c7c2d84df4cc.png"
   });
 
   eleventyConfig.ignores.add("src/pages/**");
